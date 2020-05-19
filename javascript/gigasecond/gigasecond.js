73659e8c2898b2b690b79a7e9c0fe7ff55e0ddc3
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-19 11:05:34 -0300

    Gigasecond JS

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (date) => {
+  var gs = new Date(date.getTime() + Math.pow(10,12));
+  return gs; 
 };
