66b7ff1362810cefb5725678cf7817697ac6fe13
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-25 14:41:03 -0300

    ArmstrongNumber JS

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,8 +1,7 @@
-//
-// This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const isArmstrongNumber = (n = '') => 
+   n == n.toString()
+    .split('')
+    .map((x) => Math.pow(parseInt(x), n.toString().length))
+    .reduce((acc, n) => (acc += n));
+;
