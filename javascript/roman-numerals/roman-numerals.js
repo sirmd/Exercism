882f3c2ef82273b6fb68779a33d002710df36b0f
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-25 21:45:05 -0300

    Roman-Numerals JS

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,8 +1,67 @@
-//
-// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const conversions = [
+  {
+    number: ({ n }) => n >= 1000,
+    conversion: (n) => 'M'.repeat(Math.floor(n / 1000)),
+    remainder: (n) => n - Math.floor(n / 1000) * 1000
+  },
+  {
+    number: ({ n }) => n >= 900 && n < 1000,
+    conversion: (n) => 'CM',
+    remainder: (n) => n - 900
+  },
+  {
+    number: ({ n }) => n >= 500,
+    conversion: (n) => 'D'.repeat(Math.floor(n / 500)),
+    remainder: (n) => n - Math.floor(n / 500) * 500
+  },
+  {
+    number: ({ n }) => n >= 400 && n < 500,
+    conversion: (n) => 'CD',
+    remainder: (n) => n - 400
+  },
+  {
+    number: ({ n }) => n >= 100,
+    conversion: (n) => 'C'.repeat(Math.floor(n / 100)),
+    remainder: (n) => n - Math.floor(n / 100) * 100
+  },
+  {
+    number: ({ n }) => n >= 90 && n < 100,
+    conversion: (n) => 'XC',
+    remainder: (n) => n - 90
+  },
+  {
+    number: ({ n }) => n >= 50,
+    conversion: (n) => 'L'.repeat(Math.floor(n / 50)),
+    remainder: (n) => n - Math.floor(n / 50) * 50
+  },
+  {
+    number: ({ n }) => n >= 40 && n < 50,
+    conversion: (n) => 'XL',
+    remainder: (n) => n - 40
+  },
+  {
+    number: ({ n }) => n >= 9,
+    conversion: (n) =>
+      'I'.repeat(n < 10 ? 10 - n : 0) +
+      'X'.repeat(n < 10 ? 10 - n : Math.floor(n / 10)),
+    remainder: (n) => n < 10 ? 0 : n - Math.floor(n / 10) * 10
+  },
+  {
+    number: ({ n }) => n >= 4 && n <= 8,
+    conversion: (n) => 'I'.repeat(n < 5 ? 5 - n : 0) + 'V',
+    remainder: (n) => n < 5 ? 0 : n - Math.floor(n / 5) * 5
+  },
+  {
+    number: ({ n }) => n >= 1 && n <= 3,
+    conversion: (n) => 'I'.repeat(Math.floor(n / 1)),
+    remainder: (n) => 0
+  },
+]
+export const toRoman = (n) => {
+  var conversion = '';
+  conversion += conversions.find(({ number }) => number({ n })).conversion(n);
+  var remainder = conversions.find(({ number }) => number({ n })).remainder(n);
+  if (remainder > 0) conversion += toRoman(remainder);
 
-export const toRoman = () => {
-  throw new Error("Remove this statement and implement this function");
+  return conversion;
 };
